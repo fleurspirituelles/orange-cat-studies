fb642ab65fb353592ba50a429c99f4658194e485
talitha <talitha.vieira@aluno.ifsp.edu.br>
2025-05-09 17:48:37 -0300

    refactor: update database connection methods

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,24 @@
-const mysql = require("mysql2/promise");
-require("dotenv").config();
+const mysql = require("mysql2");
 
-const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
   database: "purrfect_studies",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
-module.exports = connection;
+function connectMySQL() {
+  return new Promise((resolve, reject) => {
+    connection.connect((error) => {
+      if (error) {
+        console.error("Error connecting to MySQL:", error);
+        reject(error);
+      } else {
+        console.log("Connected to MySQL successfully.");
+        resolve(connection);
+      }
+    });
+  });
+}
+
+module.exports = connectMySQL;
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,24 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const connectMongo = require("./config/mongo");
+const connectMySQL = require("./config/database");
 const tirinhaRoutes = require("./routes/tirinha.routes");
-
-dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use("/tirinhas", tirinhaRoutes);
 
-app.use("/api/tirinhas", tirinhaRoutes);
+const port = 3000;
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB!"))
-  .catch((err) => console.error("Error connecting to MongoDB:", err));
+async function startServer() {
+  try {
+    await connectMongo();
+    await connectMySQL();
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}.`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+  }
+}
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}.`);
-});
+startServer();
