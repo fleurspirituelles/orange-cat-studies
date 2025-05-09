bf16d29fd6197af9cc5c4eb67a874c11fbb31f7b
talitha <talitha.vieira@aluno.ifsp.edu.br>
2025-05-09 19:15:31 -0300

    feat: implement user model, controller, and routes

---
+++ backend/models/user.model.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database/mysql/database");
+
+const User = sequelize.define(
+  "users",
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = User;
