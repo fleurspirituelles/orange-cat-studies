34e819bc2b3b52348553d9e267cb2a49059dea4a
talitha <talitha.vieira@aluno.ifsp.edu.br>
2025-05-09 20:35:47 -0300

    refactor: improve code structure in comic controller and model

--- backend/routes/comic.routes.js
+++ backend/routes/comic.routes.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const controller = require("../controllers/comic.controller");
 
 router.get("/", controller.getAll);
-router.post("/", controller.create);
 router.get("/:id", controller.getById);
+router.get("/code/:code", controller.getByCode);
+router.get("/user/:id_user", controller.getByUser);
+router.get("/album/:id_album", controller.getByAlbum);
+router.post("/", controller.create);
 router.put("/:id", controller.update);
 router.delete("/:id", controller.remove);
 
