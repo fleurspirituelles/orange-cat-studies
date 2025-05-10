4d8c946c048cbc2775d49cb40a1bdd6e5251075d
talitha <talitha.vieira@aluno.ifsp.edu.br>
2025-05-10 00:24:42 -0300

    style: standardize string quotes in mongo.js

--- backend/config/mongo.js
+++ backend/config/mongo.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 async function connectMongo() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/purrfect_studies');
-    console.log('Connected to local MongoDB successfully.');
+    await mongoose.connect("mongodb://localhost:27017/purrfect_studies");
+    console.log("Connected to local MongoDB successfully.");
   } catch (error) {
-    console.error('Error connecting to local MongoDB:', error);
+    console.error("Error connecting to local MongoDB:", error);
   }
 }
 
