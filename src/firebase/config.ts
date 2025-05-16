9ea36122b457e25772098503e368a8a1aa7e6bf2
talitha <talitha.vieira@aluno.ifsp.edu.br>
2025-05-16 16:48:15 -0300

    feat: add firebase configuration and authentication setup

---
+++ src/firebase/config.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgxS38uo1Hx6yU_nl3b8W_ztNZ5UOMxs4",
+  authDomain: "purr-529da.firebaseapp.com",
+  projectId: "purr-529da",
+  storageBucket: "purr-529da.appspot.com",
+  messagingSenderId: "1063632010533",
+  appId: "1:1063632010533:web:889b30a4c60e5cbc97aecc",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
