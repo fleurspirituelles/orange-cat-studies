f5d68f827473838cef99c991825d25e3768631fb
talitha <talitha.vieira@aluno.ifsp.edu.br>
2025-05-23 19:56:27 -0300

    feat: add routes for exam components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AuthPage from "./pages/AuthPage";
+import AddExamPage from "./pages/AddExamPage";
+import ManualExamForm from "./pages/ManualExamForm";
 import ProtectedRoute from "./lib/ProtectedRoute";
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/auth" element={<AuthPage />} />
+      <Route path="/add-exam" element={<AddExamPage />} />
+      <Route path="/add-exam/manual" element={<ManualExamForm />} />
     </Routes>
   );
 }
