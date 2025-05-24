fa15a8549087146dfac2f73d7ac0a18aaf6a8aef
talitha <talitha.vieira@aluno.ifsp.edu.br>
2025-05-24 01:12:23 -0300

    feat: add exam routes to the server for handling exam-related requests

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import examRoutes from "./routes/exam.routes.js";
 
 const app = express();
 const PORT = 5000;
@@ -11,7 +12,8 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/exams", examRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}.`);
 });
