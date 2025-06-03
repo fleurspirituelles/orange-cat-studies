f9fc881276ee5ac4b9ecfafd64887b0df99f10a3
talitha <talitha.vieira@aluno.ifsp.edu.br>
2025-06-03 17:10:39 -0300

    chore: update package.json to use es modules and configure testing with jest

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import examRoutes from "./routes/exam.routes.js";
+import questionRoutes from "./routes/question.routes.js";
 
 const app = express();
 const PORT = 5000;
@@ -13,7 +14,12 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/exams", examRoutes);
+app.use("/questions", questionRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}.`);
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}.`);
+  });
+}
+
+export default app;
