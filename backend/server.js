2983ff7abb8b30bbeb53e48de4c38f5340713d06
talitha <talitha.vieira@aluno.ifsp.edu.br>
2025-06-07 20:07:14 -0300

    feat: add additional route handlers

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 import express from "express";
 import cors from "cors";
+
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import examRoutes from "./routes/exam.routes.js";
+import albumRoutes from "./routes/album.routes.js";
+import answerRoutes from "./routes/answer.routes.js";
+import choiceRoutes from "./routes/choice.routes.js";
+import comicRoutes from "./routes/comic.routes.js";
+import performanceRoutes from "./routes/performance.routes.js";
+import questionRoutes from "./routes/question.routes.js";
+import reviewRoutes from "./routes/review.routes.js";
+import topicRoutes from "./routes/topic.routes.js";
 
 const app = express();
 const PORT = 5000;
@@ -13,6 +22,14 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/exams", examRoutes);
+app.use("/albums", albumRoutes);
+app.use("/answers", answerRoutes);
+app.use("/choices", choiceRoutes);
+app.use("/comics", comicRoutes);
+app.use("/performance", performanceRoutes);
+app.use("/questions", questionRoutes);
+app.use("/reviews", reviewRoutes);
+app.use("/topics", topicRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}.`);
