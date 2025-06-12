ccb7f6661a5aad09a8986194d33869ec0aee5fe7
talitha <talitha.vieira@aluno.ifsp.edu.br>
2025-06-12 19:41:23 -0300

    feat: add dockerfile and docker-compose for backend setup

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,17 @@
 import mysql from "mysql2/promise";
 
+const {
+  DB_HOST = "localhost",
+  DB_USER = "root",
+  DB_PASSWORD = "root",
+  DB_NAME = "purrfect_studies",
+} = process.env;
+
 const connection = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "purrfect_studies",
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
 });
 
 export default connection;
